4f91d480f3c77146165b4d2c526ba2d499236432
Kleber Santos <kleber.santos@sefaz.rr.gov.br>
2020-12-11 12:27:39 -0400

    Testes unitarios da aplicacao

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,27 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DateUtilsTest {
+
+    @Test
+    public void mustReturnTrueToFutureDates(){
+        LocalDate date = LocalDate.of(2030, 1, 1);
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void mustReturnFalseToPastDates(){
+        LocalDate date = LocalDate.of(2010, 1, 1);
+        Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void mustReturnTrueToNow(){
+        LocalDate date = LocalDate.now();
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+}
